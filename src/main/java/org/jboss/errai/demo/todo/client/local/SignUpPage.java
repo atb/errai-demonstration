b29bb94110c57f2d0c14924aa9b79d0cfab172a6
Alexandre Braganca <atb@isep.ipp.pt>
2019-01-08 18:27:50 +0000

    Added async call to jax-rs service.

--- src/main/java/org/jboss/errai/demo/todo/client/local/SignUpPage.java
+++ src/main/java/org/jboss/errai/demo/todo/client/local/SignUpPage.java
@@ -1,7 +1,13 @@
 package org.jboss.errai.demo.todo.client.local;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
+import org.jboss.errai.demo.todo.client.shared.Contact;
+import org.jboss.errai.demo.todo.client.shared.ContactService;
 import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.nav.client.local.TransitionTo;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
@@ -9,6 +15,7 @@ import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 
@@ -23,8 +30,22 @@ public class SignUpPage extends Composite {
 	@Inject
 	TransitionTo<LoginForm> goToLoginForm;
 	
+	@Inject
+	private Caller<ContactService> contactService;
+	
 	@EventHandler("signup1")
 	public void clickSignup(ClickEvent e) {
+		
+		// Lets try to call a service in the server:
+		RemoteCallback<List<Contact>> callback = new RemoteCallback<List<Contact>>() {
+
+			  public void callback(List<Contact> contacts) {
+			    Window.alert("Contacts received: " + contacts.size());
+			  }
+			};
+		contactService.call(callback).listAllContacts();
+
 		goToLoginForm.go();
+		
 	}
 }

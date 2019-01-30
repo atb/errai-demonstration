c7a2d70218df13f257128d6d5bece2803da3aa4b
Alexandre Braganca <atb@isep.ipp.pt>
2019-01-30 15:45:18 +0000

    Added class to implement jpa for Contacts.

--- src/main/java/org/jboss/errai/demo/todo/client/shared/Contact.java
+++ src/main/java/org/jboss/errai/demo/todo/client/shared/Contact.java
@@ -3,6 +3,13 @@ package org.jboss.errai.demo.todo.client.shared;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.jboss.errai.databinding.client.api.Bindable;
 
@@ -13,10 +20,19 @@ import org.jboss.errai.databinding.client.api.Bindable;
  */
 @Bindable
 @Portable
+@Entity
+@NamedQueries({
+  @NamedQuery(name = Contact.ALL_CONTACTS_QUERY, query = "SELECT c FROM Contact c ORDER BY c.id")
+})
 public class Contact implements Serializable, Comparable<Contact> {
   private static final long serialVersionUID = 1L;
+  
+  public static final String ALL_CONTACTS_QUERY = "allContacts";  
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private long id;
+  
   private String firstname;
 
   public Contact() {}
@@ -55,5 +71,15 @@ public class Contact implements Serializable, Comparable<Contact> {
   @Override
   public int compareTo(Contact contact) {
     return (int) (id - contact.id);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    return (obj instanceof Contact) && ((Contact) obj).getId() != 0 && ((Contact) obj).getId() == getId();
+  }
+
+  @Override
+  public int hashCode() {
+    return (int) getId();
   }
 }

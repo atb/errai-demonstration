3263b7cd6bc27e4b618cf3489b553fbb36e39d27
Alexandre Braganca <atb@isep.ipp.pt>
2019-02-20 10:11:45 +0000

    TEST: Add simple void example test.

---
+++ src/test/java/org/atb/errai/demo/contactlist/ExampleJRETest.java
@@ -0,0 +1,12 @@
+package org.atb.errai.demo.contactlist;
+
+import junit.framework.TestCase;
+
+public class ExampleJRETest extends TestCase {
+
+	
+	public void testVoid() {
+		// TODO
+		assertTrue(true);
+	}	
+}

d9eba749e508408492b0cc8232a801c436d4d50d
Alexandre Braganca <atb@isep.ipp.pt>
2019-03-06 18:41:26 +0000

    ADD: Small unit test.

---
+++ src/test/java/org/atb/errai/demo/contactlist/client/shared/ContactTest.java
@@ -0,0 +1,14 @@
+package org.atb.errai.demo.contactlist.client.shared;
+
+import junit.framework.TestCase;
+
+public class ContactTest extends TestCase {
+
+
+	public void testAttributtesAreValid() {
+		// TODO
+		Contact c=new Contact();
+		
+		assertTrue(c != null);
+	}	
+}
